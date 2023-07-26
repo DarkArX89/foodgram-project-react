53d7a1b09f3080a0e35b05a9bc18e32884f3b306
Иван Михайлец <dark_arx@mail.ru>
2023-07-26 19:06:47 +0500

    Fix bugs

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -68,7 +68,7 @@ class Recipe(models.Model):
         'Дата публикации', default=timezone.now)
 
     class Meta:
-        ordering = ['pub_date']
+        ordering = ['-pub_date']
         verbose_name = 'Рецепт'
         verbose_name_plural = 'Рецепты'
         constraints = [models.UniqueConstraint(
